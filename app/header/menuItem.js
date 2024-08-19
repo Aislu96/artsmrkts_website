746f34938b8e734da8aa35a0aba984e40904e9c2
aislusha96 <arslanova1996ais@list.ru>
2024-08-19 18:55:39 +0300

    header

---
+++ app/header/menuItem.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const MenuItem = ({ label, isActive, handleClick, links = [] }) => {
+    return (
+        <div className="relative">
+            <div
+                onClick={handleClick}
+                className={`flex flex-row items-center px-4 py-2 gap-3 cursor-pointer ${
+                    isActive ? 'bg-customOrangeBg rounded-[44px]' : ''
+                }`}
+            >
+                <div className="flex flex-row items-center">
+                    <span
+                        className={`text-[16px] leading-[19.2px]  ${isActive ? 'text-customRedText' : ''}`}>{label}</span>
+                </div>
+                <div className="w-2.5 h-2.5 flex items-center justify-center">
+                    <Image
+                        src={isActive ? "arrowRed.svg" : "arrow.svg"}
+                        alt="arrow"
+                        width={7}
+                        height={4}
+                        className={`object-contain transition-transform ${
+                            isActive ? "rotate-180" : ""
+                        }`}
+                    />
+                </div>
+            </div>
+
+            {isActive && (
+                <div className="absolute top-[43px] left-0 box-shadow rounded-lg flex flex-col w-full bg-white z-10">
+                    {links.map((link, index) => (
+                        <Link key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="px-4 py-1 hover:text-customOrange active:text-customRedText border-bottom">
+                            <span className="text-[14px] leading-[16.8px]">{link.text}</span>
+                        </Link>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default MenuItem;
---
+++ tailwind.config.js
@@ -0,0 +1,87 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js, jsx,ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1440px",
+      },
+    },
+    extend: {
+      colors: {
+        customHeaderFont: "#0C1227",
+        customGrey: "#E8E8E9",
+        customGreyTwo: "#F6F6F6",
+        customGreyThree: "#939393",
+        customBlue: "#182654",
+        customBlueTwo: "#001240",
+        customBlueFon: "#0F2777",
+        customRedText: "#851000",
+        customOrange: "#DC4016",
+        customOrangeBg: "#FFCEBD",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
